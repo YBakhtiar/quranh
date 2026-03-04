3bd00a220cdef5372819454e49be42f5c0fef484
YBakhtiar <121402280+YBakhtiar@users.noreply.github.com>
2026-03-04 15:32:09 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'quran-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+  // فایل‌های CSS یا فونت اگر محلی هستند اینجا اضافه شوند
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
